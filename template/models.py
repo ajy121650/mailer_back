b6527c0d0263139e05ea1333e20f9d653f563aec
ajy1216 <ajy1216@snu.ac.kr>
2025-11-01 17:15:08 +0900

    feat: make migrations

--- template/models.py
+++ template/models.py
@@ -10,13 +10,15 @@ from email_account.models import EmailAccount
 # Create your models here.
 class Template(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="templates")
-    email_account = models.ForeignKey(EmailAccount, on_delete=models.CASCADE, related_name="templates", blank=True)
-    template_content = models.CharField(max_length=255)
-    template_title = models.CharField(max_length=255, default="")
-    main_category = models.CharField(max_length=100)
-    sub_category = models.CharField(max_length=100)
-    topic = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    email_account = models.ForeignKey(
+        EmailAccount, on_delete=models.CASCADE, related_name="templates", null=True, blank=True
+    )
+    template_content = models.CharField(max_length=1000, default="", null=True, blank=True)
+    template_title = models.CharField(max_length=255, default="", null=True, blank=True)
+    main_category = models.CharField(max_length=100, default="", null=True, blank=True)
+    sub_category = models.CharField(max_length=100, default="", null=True, blank=True)
+    topic = models.CharField(max_length=100, default="", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return f"Template#{self.id} of {self.email_account.address}"
