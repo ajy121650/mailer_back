1c74c4561d5b5da5e933035d5e717480247f4e2c
dongin <dongin1001@naver.com>
2025-11-24 12:00:47 +0900

    메일 동기화 이슈 수정

--- user/auth.py
+++ user/auth.py
@@ -1,6 +1,7 @@
 import requests
 from functools import lru_cache
 from jose import jwt
+from jose.backends.rsa_backend import RSAKey
 from django.conf import settings
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
 from rest_framework import authentication, exceptions
@@ -20,7 +21,7 @@ def _get_public_key(token):
     for key in _fetch_jwks().get("keys", []):
         if key.get("kid") == kid:
             # jose가 JWK를 키 객체로 바꿔서 쓸 수 있게 해줌
-            return jwt.algorithms.RSAAlgorithm.from_jwk(key)
+            return RSAKey(key, algorithm="RS256")
     raise exceptions.AuthenticationFailed("Public key not found for token")
 
 
@@ -62,9 +63,22 @@ class ClerkAuthentication(authentication.BaseAuthentication):
         return (user, payload)
 
 
+class ClerkAuthenticationScheme(OpenApiAuthenticationExtension):
+    target_class = "user.auth.ClerkAuthentication"
+    name = "clerkAuth"
+
+    def get_security_definition(self, auto_schema):
+        return {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+            "description": "Clerk JWT 토큰을 사용한 인증. Authorization 헤더에 'Bearer <token>' 형식으로 전달합니다.",
+        }
+
+
 ######################## API 테스트를 위한 임시 Authentication #############
 class TestAuthentication(authentication.BaseAuthentication):
-    """(API_TEST_MODE=True 일 때 사용) 모든 요청을 'testuser'로 자동 인증하는 테스트용 클래스"""
+    """(=True 일 때 사용) 모든 요청을 'testuser'로 자동 인증하는 테스트용 클래스"""
 
     def authenticate(self, request):
         # 무조건 'testuser'로 사용자를 찾거나, 없으면 생성
